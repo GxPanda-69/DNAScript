2e5e49030e672a403f04f1baf872f52ec9336de5
GxPanda <gx.panda.off@gmail.com>
2026-07-01 14:59:57 +0200

    Changed test script

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { Tokenizer } from "./tokenizer";
 
-let tokenizer = new Tokenizer("GAG GTG")
+let tokenizer = new Tokenizer("TCGG C AHelloA G");
 tokenizer.tokenize();
 
 tokenizer.tokens.forEach((token) => {
-  console.log(token)
+  console.log(token);
 });
