56ccea3ab70725c320a4cd28aed987696962c2de
GxPanda <gx.panda.off@gmail.com>
2026-07-02 15:19:46 +0200

    Update test script to Hello World

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { Tokenizer } from "./tokenizer";
 
-let tokenizer = new Tokenizer("TCGG C AHelloA G");
+let tokenizer = new Tokenizer("TCGG C AHello World !A G");
 tokenizer.tokenize();
 
 tokenizer.tokens.forEach((token) => {
