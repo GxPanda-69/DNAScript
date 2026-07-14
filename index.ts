b77d04c745b5f013176b520eda23260abc902241
GxPanda <gx.panda@proton.me>
2026-07-14 19:01:38 +0200

    Update test script

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
+import { Parser } from "./parser";
 import { Tokenizer } from "./tokenizer";
 
-let tokenizer = new Tokenizer("TCGG C AHello World !A G");
+let tokenizer = new Tokenizer(`
+  TCGG C AHello World !A G TCGG C AThis is a testA G`);
 tokenizer.tokenize();
 
 tokenizer.tokens.forEach((token) => {
   console.log(token);
 });
+
+let parser = new Parser(tokenizer.tokens);
+parser.parseTokens();
