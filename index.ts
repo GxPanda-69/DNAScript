21714acc54e4c898a33352a14e9e99e9712d47b7
GxPanda <gx.panda@proton.me>
2026-07-24 15:30:37 +0200

    Update index.ts to be clearer with logs and to test function declarations

--- index.ts
+++ index.ts
@@ -1,11 +1,22 @@
 import { Parser } from "./parser";
 import { Tokenizer } from "./tokenizer";
 
-let tokenizer = new Tokenizer(`
-  GC titi AA AHelloA`);
+let tokenizer = new Tokenizer(`GC HelloWorldFunc AA CT TCGG C Message G TC`);
+
+console.log("=== TOKENIZATION OUTPUT IN");
 tokenizer.tokenize();
+console.log("=== TOKENIZATION OUTPUT OUT");
+
+console.log("=== TOKENS IN ===");
+tokenizer.tokens.forEach((token) => console.log(token));
+console.log("=== TOKENS OUT ===");
 
 let parser = new Parser(tokenizer.tokens);
-parser.parseTokens();
 
-parser.ast.root.forEach((node) => console.log(node));
+console.log("=== PARSING OUTPUT IN ===");
+parser.parseTokens();
+console.log("=== PARSING OUTPUT OUT ===");
+
+console.log("=== AST IN ===");
+parser.ast.root.forEach((node) => console.dir(node, { depth: 100 }));
+console.log("=== AST OUT ===");
