d141e2de482acd98d9d3d9ff17d558070c6e8d90
GxPanda <gx.panda@proton.me>
2026-07-25 22:15:08 +0200

    Make tokens numbered in output + reformatted test script.

--- index.ts
+++ index.ts
@@ -2,16 +2,27 @@ import { Interpreter } from "./interpreter";
 import { Parser } from "./parser";
 import { Tokenizer } from "./tokenizer";
 
-let tokenizer = new Tokenizer(
-  `GC MessageContent AA AHello WorldA GC HelloWorldFunc AA CT C Message G TCGG C Message G TC HelloWorldFunc C MessageContent G MessageContent AA AHello World number twoA HelloWorldFunc C MessageContent G`,
-);
+let tokenizer = new Tokenizer(`
+GC MessageContent AA AHello WorldA
+GC HelloWorldFunc AA CT
+  C Message G
+  TCGG C Message G
+TC
+HelloWorldFunc C MessageContent G
+MessageContent AA AHello World number twoA
+HelloWorldFunc C MessageContent G
+`);
 
 console.log("=== TOKENIZATION OUTPUT IN");
 tokenizer.tokenize();
 console.log("=== TOKENIZATION OUTPUT OUT");
 
 console.log("=== TOKENS IN ===");
-tokenizer.tokens.forEach((token) => console.log(token));
+let idx = 0;
+tokenizer.tokens.forEach((token) => {
+  idx++;
+  console.log(idx, token);
+});
 console.log("=== TOKENS OUT ===");
 
 let parser = new Parser(tokenizer.tokens);
