cc8d5e42bd951c7a76105477816d6eaa4271ae0d
GxPanda <gx.panda.off@gmail.com>
2026-07-31 15:48:04 +0200

    Add execute.ts and execute npm script

---
+++ execute.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from "node:fs";
+import { Interpreter } from "./interpreter";
+import { Parser } from "./parser";
+import { Tokenizer } from "./tokenizer";
+
+const code = process.argv[2];
+
+if (!code) {
+  console.error('Usage: npx ts-node execute.ts "<script to execute>"');
+  process.exit(1);
+}
+
+const tokenizer = new Tokenizer(code);
+tokenizer.tokenize();
+
+const parser = new Parser(tokenizer.tokens);
+parser.parseTokens();
+
+const interpreter = new Interpreter();
+interpreter.Execute(parser.ast);
